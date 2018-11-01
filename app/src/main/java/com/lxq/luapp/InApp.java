4e44250644cba57094b9bbabc66eb4e114fdf752
young1293 <2457471549@qq.com>
2018-11-01 22:45:09 +0800

    第一次提交

---
+++ app/src/main/java/com/lxq/luapp/InApp.java
@@ -0,0 +1,11 @@
+package com.lxq.luapp;
+
+import android.app.Application;
+
+public class InApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+    
+}
---
+++ app/src/main/java/com/lxq/luapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.lxq.luapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lxq.luapp.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    public int getlayoutId() {
+
+        return R.layout.activity_main;
+
+    }
+
+    @Override
+    public void initView() {
+        
+    }
+
+    @Override
+    public void initData() {
+
+    }
+    
+
+
+}
